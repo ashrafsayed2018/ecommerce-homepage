9920c17980cb966929db3b562941297c5b9a3e6f
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2024-05-20 18:06:51 +0300

    finishing the product card

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -58,29 +58,5 @@ export default function ProductCard({ product }) {
         </div>
       </Link>
     </div>
-    // <div>
-    //   <Link href={`/product/${product._id}`} className="w-full mb-16">
-    //     <div className="image">
-    //       <Image
-    //         src={product.imageUrl}
-    //         alt={product.description}
-    //         width={400}
-    //         height={400}
-    //         placeholder="blur"
-    //         blurDataURL={product.imageUrl}
-    //         className="w-full h-[600px] sm:h-screen"
-    //       />
-    //     </div>
-    //     <div className="text-center mt-8 flex flex-col gap-y-2">
-    //       <p>{product.description}</p>
-    //       <p className="text-sm font-thin text-gray-500">{product.category}</p>
-    //       <p className="italic">
-    //         د.ك
-    //         {"  "} {product.price}
-    //       </p>
-    //     </div>
-    //   </Link>
-    //   {/* <CartButton product={product} /> */}
-    // </div>
   );
 }
